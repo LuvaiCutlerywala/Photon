1db0d4bdf8a0b418e40d8cf7ac2d6e6230f9cd2c
Luvai Cutlerywala <64072072+LuvaiCutlerywala@users.noreply.github.com>
2025-01-06 18:58:19 +0000

    Added reader to read file, and strip comments.

---
+++ src/include/reader.h
@@ -0,0 +1,8 @@
+//
+// Created by Luvai Cutlerywala on 06/01/2025.
+//
+
+#ifndef READER_H
+#define READER_H
+
+#endif //READER_H
---
+++ src/reader.c
@@ -0,0 +1,5 @@
+//
+// Created by Luvai Cutlerywala on 06/01/2025.
+//
+
+#include "reader.h"
