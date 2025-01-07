b9a8f55676724042f854590abbe39009e00bdd5f
Luvai Cutlerywala <64072072+LuvaiCutlerywala@users.noreply.github.com>
2025-01-07 20:04:15 +0000

    Added AST node.

---
+++ src/AST.c
@@ -0,0 +1,16 @@
+#include "AST.h"
+
+AST_NODE* init_ast_node(AST_TYPE type) {
+  AST_NODE* node = calloc(1, sizeof(AST_NODE));
+  node->type = type;
+  node->variable_defined_name = NULL;
+  node->variable_definition = NULL;
+  node->variable_name = NULL;
+  node->function_call_name = NULL;
+  node->function_call_args = NULL;
+  node->function_call_args = NULL;
+  node->args_size = 0;
+  node->string = NULL;
+
+  return node;
+}
---
+++ src/include/AST.h
@@ -0,0 +1,39 @@
+#ifndef AST_H
+#define AST_H
+
+#include <stdlib.h>
+
+typedef enum ast_type {
+  AST_VARIABLE_DEFINITION,
+  AST_VARIABLE,
+  AST_FUNCTION_CALL,
+  AST_STRING,
+  AST_COMPOUND
+} AST_TYPE;
+
+typedef struct ast_node {
+  AST_TYPE type;
+
+  //AST_VARIABLE_DEFINITION
+  char* variable_defined_name;
+  struct AST_NODE* variable_definition;
+
+  //AST_VARIABLE
+  char* variable_name;
+
+  //AST_FUNCTION_CALL
+  char* function_call_name;
+  struct AST_NODE** function_call_args;
+  size_t args_size;
+
+  //AST_STRING
+  char* string;
+
+  //AST_COMPOUND
+  struct AST_NODE** compound;
+  size_t compound_size;
+} AST_NODE;
+
+AST_NODE* init_ast_node(AST_TYPE type);
+
+#endif
