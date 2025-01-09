dcbcceaa81935baf72c40522958a6621024402a8
Luvai Cutlerywala <64072072+LuvaiCutlerywala@users.noreply.github.com>
2025-01-09 09:32:23 +0000

    Added comments to the reader header file.

--- src/include/reader.h
+++ src/include/reader.h
@@ -1,6 +1,8 @@
 #ifndef READER_H
 #define READER_H
 
+/// @brief Reads the contents of the file into one large character buffer.
+/// @param filename The relative path of the file.
 char* read_file(const char* filename);
 
 #endif
