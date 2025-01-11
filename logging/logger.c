5cd48bb6f18544bf89be08579705edf0e58bc451
Luvai Cutlerywala <64072072+LuvaiCutlerywala@users.noreply.github.com>
2025-01-11 10:35:33 +0000

    Added methods to log numbers and comments.

--- logging/logger.c
+++ logging/logger.c
@@ -1,16 +1,26 @@
 #include "logger.h"
 
 #include <time.h>
-#include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
-void print(FILE* stream, char* mode, char* message, char* source) {
+char* get_timestamp() {
   const time_t current_time = time(NULL);
   const char* ctime_timestamp = ctime(&current_time);
-  char* timestamp = malloc(sizeof(char) * (strlen(ctime_timestamp) - 2));
-  memcpy(timestamp, ctime_timestamp, strlen(ctime_timestamp) - 2);
+  char* timestamp = malloc(sizeof(char)* (strlen(ctime_timestamp) - 2));
+  memcpy(timestamp, ctime_timestamp, strlen(ctime_timestamp));
+  return timestamp;
+}
+
+void print(FILE* stream, char* mode, char* message, char* source) {
+  char* timestamp = get_timestamp();
   fprintf(stream, "[%s] [%s] AT [%s]: %s\n", mode, timestamp, source, message);
+  free(timestamp);
+}
+
+void print(FILE* stream, char* mode, int value, char* source) {
+  char* timestamp = get_timestamp();
+  fprintf(stream, "[%s] [%s] AT [%s]: %d\n", mode, timestamp, source, value);
   free(timestamp);
 }
 
@@ -18,14 +28,30 @@ void info(char* source, char* message){
   print(stdout, "INFO", message, source);
 }
 
+void info(char* source, int value) {
+  print(stdout, "INFO", value, source);
+}
+
 void error(char* source, char* message){
   print(stderr, "ERROR", message, source);
+}
+
+void error(char* source, int value) {
+  print(stderr, "ERROR", value, source);
 }
 
 void warn(char* source, char* message){
   print(stderr, "WARN", message, source);
 }
 
+void warn(char* source, int value) {
+  print(stderr, "WARN", value, source);
+}
+
 void debug(char* source, char* message){
   print(stdout, "DEBUG", message, source);
+}
+
+void debug(char* source, int value) {
+  print(stdout, "DEBUG", value, source);
 }
--- logging/logger.h
+++ logging/logger.h
@@ -1,12 +1,64 @@
 #ifndef LOGGER_H
 #define LOGGER_H
 
+#include <stdio.h>
+
+/// @brief Returns the non-terminated current timestamp for logging.
+char* get_timestamp(void);
+
+/// @brief Prints a string message to the designated output.
+/// @param stream The designated output.
+/// @param mode The logging mode. Can be either info, warn, error, or debug.
+/// @param message The string message to be logged.
+/// @param source The location where the log originates.
+void print(FILE* stream, char* mode, char* message, char* source);
+
+/// @brief Prints an integer to the designated output.
+/// @param stream The designated output.
+/// @param mode The logging mode. Can be either info, warn, error, or debug.
+/// @param value The integer value to be logged.
+/// @param source The location where the log originates.
+void print(FILE* stream, char* mode, int value, char* source);
+
+/// @brief Logs a piece of information to the standard output.
+/// @param source The location where the log originates.
+/// @param message The message to be logged.
 void info(char* source, char* message);
 
+/// @brief Logs an integral piece of information to the standard output.
+/// @param source The location where the log originates.
+/// @param value The integral value to be logged.
+void info(char* source, int value);
+
+/// @brief Logs an error to the standard error output.
+/// @param source The location where the log originates.
+/// @param message The message to be logged.
 void error(char* source, char* message);
 
+/// @brief Logs an integral error to the standard error output.
+/// @param source The location where the log originates.
+/// @param value The integral value to be logged.
+void error(char* source, int value);
+
+/// @brief Logs a warning to the standard error output.
+/// @param source The location where the log originates.
+/// @param message The message to be logged.
 void warn(char* source, char* message);
 
+/// @brief Logs an integral warning to the standard error output.
+/// @param source The location where the log originates.
+/// @param value The integral value to be logged.
+void warn(char* source, int value);
+
+/// @brief Logs a piece of debugging information to the standard output.
+/// @param source The location where the log originates.
+/// @param message The message to be logged.
 void debug(char* source, char* message);
+
+/// @brief Logs an integral piece of debugging information to the standard output.
+/// @param source The location where the log originates.
+/// @param value The integral value to be logged.
+void debug(char* source, int value);
+
 
 #endif
