ea680ff8e0667ba1e8715f951aafe991cd81ba70
Luvai Cutlerywala <64072072+LuvaiCutlerywala@users.noreply.github.com>
2025-01-16 17:47:49 +0000

    Added util function.

--- src/include/macros.h
+++ src/include/macros.h
@@ -1,9 +1,15 @@
 #ifndef MACROS_H
 #define MACROS_H
 
+#include <stdlib.h>
+
 /*
  * Gets the size of the array.
  */
 #define SIZE(arr) (sizeof(arr) / sizeof(arr[0]))
+
+/// @brief Wraps a single character into a string.
+/// @param character The character to be wrapped.
+char* wrap_as_str(const char character);
 
 #endif
