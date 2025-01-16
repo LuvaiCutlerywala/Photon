10ce4aaaa9e2e3e7b79adbeab460cea1864cfc4f
Luvai Cutlerywala <64072072+LuvaiCutlerywala@users.noreply.github.com>
2025-01-16 18:02:58 +0000

    Added support for basic arithmetic operations.

--- internal/internal_operations.c
+++ internal/internal_operations.c
@@ -1,1 +1,36 @@
 #include "internal_operations.h"
+
+#include <limits.h>
+#include <stdlib.h>
+#include <logging/logger.h>
+
+long internal_op_add(long a, long b) {
+    chk_precision(a, b);
+    return a + b;
+}
+
+long internal_op_subtract(long a, long b) {
+    chk_precision(a, b);
+    return a - b;
+}
+
+long internal_op_multiply(long a, long b) {
+    chk_precision(a, b);
+    return a * b;
+}
+
+long internal_op_divide(long a, long b) {
+    chk_precision(a, b);
+    return a / b;
+}
+
+void chk_precision(long a, long b) {
+    if (a == LONG_MAX ||
+        a == LONG_MIN ||
+        b == LONG_MAX ||
+        b == LONG_MIN)
+    {
+        error("internal_operations.chk_precision", "Precision failure, operands are max/min value for type.");
+        exit(1);
+    }
+}
--- internal/internal_operations.h
+++ internal/internal_operations.h
@@ -3,4 +3,11 @@
 
 //TODO: Create functions for basic operations in the photon language.
 
+long internal_op_add(long a, long b);
+long internal_op_subtract(long a, long b);
+long internal_op_multiply(long a, long b);
+long internal_op_divide(long a, long b);
+
+void chk_precision(long a, long b);
+
 #endif
